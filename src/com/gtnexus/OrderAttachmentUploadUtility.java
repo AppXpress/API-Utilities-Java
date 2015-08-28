c030c9d15b2b0af6e0fa0f3eae9737fc6796e016
Henry Lu <henry.lu@gtnexus.com>
2015-08-28 15:25:38 -0400

    Initial commit

---
+++ src/com/gtnexus/OrderAttachmentUploadUtility.java
@@ -0,0 +1,82 @@
+package com.gtnexus;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.nio.file.Files;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class OrderAttachmentUploadUtility {
+    // Version of API
+    final static String version = "310";
+    // URL of Environment
+    final static String baseURL = "https://commerce-demo.gtnexus.com/rest/" + version;
+    // Your user's authorization token
+    private static String authorization;
+    // Your organization's data key
+    private static String dataKey;
+
+    public static void main(String[] args) {
+        // Retrieve the authorization token, data key, order folder's uid
+        // and attachment file name from arguments.
+        authorization = args[0];
+        dataKey = args[1];
+        String orderUid = args[2];
+        String filename = args[3];
+        uploadAttachment(orderUid, filename);
+    }
+
+    private static void uploadAttachment(String orderUid, String filename)
+    {
+        try {
+            String urlString = baseURL + "/OrderDetail/" + orderUid + "/attach?dataKey=" + dataKey;
+            URL url = new URL(urlString);
+
+            // Open a HTTP connection to the URL
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // Allow Inputs
+            conn.setDoInput(true);
+            // Allow Outputs
+            conn.setDoOutput(true);
+            // Don't use a cached copy.
+            conn.setUseCaches(false);
+            // Use a post method.
+            conn.setRequestMethod("POST");
+
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("Authorization", authorization);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Content-Disposition",
+                    "attachment;filename=\"" + filename + "\"");
+
+            // Send the file content to the output stream.
+            DataOutputStream dataOutputStream = new DataOutputStream(conn.getOutputStream());
+            File file = new File(filename);
+            Files.copy(file.toPath(), dataOutputStream);
+            dataOutputStream.flush();
+            dataOutputStream.close();
+
+            // Expect a 201 success code, or else print out the error.
+            int responseCode = conn.getResponseCode();
+            if (responseCode == 201) {
+                System.out.print("File was uploaded successfully.");
+
+            } else {
+                InputStream inputStream = conn.getInputStream();
+                byte[] buffer = new byte[2048];
+                int length;
+                while ((length = inputStream.read(buffer)) != -1) {
+                    System.out.println(new String(buffer, 0, length));
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/com/gtnexus/OrderAttachmentsDownloadUtility.java
@@ -0,0 +1,123 @@
+package com.gtnexus;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+// JSON library from https://github.com/douglascrockford/JSON-java
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class OrderAttachmentsDownloadUtility {
+    // Version of API
+    private final static String version = "310";
+    // URL of Environment
+    private final static String baseURL = "https://commerce-demo.gtnexus.com/rest/" + version;
+    // Your user's authorization token
+    private static String authorization;
+    // Your organization's data key
+    private static String dataKey;
+
+    public static void main(String[] args) {
+        // Retrieve the authorization token, data key and order folder's uid from arguments.
+        authorization = args[0];
+        dataKey = args[1];
+        String orderUid = args[2];
+
+        // Step 1 - Get the attachment list of the order.
+        JSONArray attachmentList = getAttachmentList(orderUid);
+
+        // Step 2 - Download each attachment file.
+        for (int i = 0; i < attachmentList.length(); i++) {
+            JSONObject attachment = attachmentList.getJSONObject(i);
+            String attachmentUid = attachment.getString("attachmentUid");
+            String filename = attachment.getString("name");
+            downloadAttachment(attachmentUid, filename);
+        }
+    }
+
+    public static JSONArray getAttachmentList(String orderUid) {
+        try {
+            String urlString = baseURL + "/OrderDetail/" + orderUid + "/attachment" +
+                    "?dataKey=" + dataKey;
+            URL url = new URL(urlString);
+
+            // Set up the HTTP GET request.
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Connection", "Close");
+            conn.setRequestProperty("Authorization", authorization);
+            conn.setRequestProperty("Accept", "application/json");
+
+            int responseCode = conn.getResponseCode();
+            if (responseCode == 200) {
+                BufferedReader responseBuffer =
+                        new BufferedReader(new InputStreamReader((conn.getInputStream())));
+                StringBuilder responseStrBuilder = new StringBuilder();
+                String inputStr;
+                while ((inputStr = responseBuffer.readLine()) != null) {
+                    responseStrBuilder.append(inputStr);
+                }
+
+                // Convert the response body into a JSONObject.
+                JSONObject response = new JSONObject(responseStrBuilder.toString());
+                // Return the result[] array in the response body.
+                return response.getJSONArray("result");
+
+            } else {
+                return new JSONArray();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return new JSONArray();
+        }
+    }
+
+
+    public static void downloadAttachment(String attachmentUid, String filename) {
+        try {
+            String urlString =  baseURL + "/media/" + attachmentUid +
+                    "?dataKey=" + dataKey;
+            URL url = new URL(urlString);
+
+            // Set up the HTTP GET request.
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Connection", "Close");
+            conn.setRequestProperty("Authorization", authorization);
+
+            // Expect a 200 success code, or else print out the error.
+            int responseCode = conn.getResponseCode();
+            if (responseCode == 200) {
+
+                InputStream inputStream = conn.getInputStream();
+                OutputStream outputStream = new FileOutputStream(filename);
+                byte[] buffer = new byte[2048];
+                int length;
+                while ((length = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, length);
+                }
+
+                inputStream.close();
+                outputStream.close();
+
+            } else {
+
+                InputStream inputStream = conn.getInputStream();
+                byte[] buffer = new byte[2048];
+                int length;
+                while ((length = inputStream.read(buffer)) != -1) {
+                    System.out.println(new String(buffer, 0, length));
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
